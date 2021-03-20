e021fa172dfd0301f5f86dd8da320418aacabce1
Sıtkı Bağdat <sbagdat@gmail.com>
2021-03-20 03:02:02 +0300

    Visitor can see all information about a specific shelter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :shelters, only: [:index]
+  resources :shelters, only: [:index, :show]
 end
---
+++ spec/features/shelters/show_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "shelter show page", type: :feature do
+  it "can see all information about that specific shelter" do
+    shelter = create(:shelter)
+
+    visit "shelters/#{shelter.id}"
+
+    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(shelter.city)
+    expect(page).to have_content(shelter.rank)
+  end
+end
