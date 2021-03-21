2423576157772cf240e8c86db49131a8d41076aa
Sıtkı Bağdat <sbagdat@gmail.com>
2021-03-21 04:22:27 +0300

    Visitors can see count of pets for each shelters

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe "shelter show page", type: :feature do
     expect(page).to have_content(shelter.city)
     expect(page).to have_content(shelter.rank)
   end
+
+  it "can see number of adopted pets" do
+    shelter = create(:shelter) do |s|
+      create_list(:pet, 3, shelter: s)
+    end
+
+    visit "shelters/#{shelter.id}"
+
+    expect(page).to have_content("Pets Count: #{shelter.pets.length}")
+  end
 end
