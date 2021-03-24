58ca3b0014d5ae3e5db17b02a80bcd30cba73054
Sıtkı Bağdat <sbagdat@gmail.com>
2021-03-24 04:34:42 +0300

    sort shelters by number of pets desc
    - style refactorings

--- app/helpers/tailwind_helper.rb
+++ app/helpers/tailwind_helper.rb
@@ -3,10 +3,17 @@
 module TailwindHelper
   BTN_CLASS = "p-2 text-md text-white border w-24 text-center rounded-md my-4 inline-block"
 
-  def link_button(text, link, classes, **options)
+  def tw_link(text, link, classes, **options)
     link_to text,
             link,
             class: "#{BTN_CLASS} #{classes}",
             **options
+  end
+
+  def tw_button(text, link, classes, **options)
+    button_to text,
+              link,
+              class: "#{BTN_CLASS} #{classes}",
+              **options
   end
 end
--- spec/features/usability/sort_shelters_by_number_of_pets_spec.rb
+++ spec/features/usability/sort_shelters_by_number_of_pets_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "sort shelters by number of pets ", type: :feature do
   describe "as a visitor" do
     describe "shelters index link" do
-      it "can sort shelters" do
+      it "can sort shelters in ascending order" do
         shelter1 = create(:shelter) do |shelter|
           create_list(:pet, 3, shelter: shelter)
         end
@@ -32,6 +32,37 @@ RSpec.describe "sort shelters by number of pets ", type: :feature do
         within ".shelters li:last-child" do
           expect(page).to have_content(shelter3.name)
           expect(page).to have_content(shelter3.pets.count)
+        end
+      end
+
+      it "can sort shelters in descending order" do
+        shelter1 = create(:shelter) do |shelter|
+          create_list(:pet, 3, shelter: shelter)
+        end
+        shelter2 = create(:shelter) do |shelter|
+          create_list(:pet, 1, shelter: shelter)
+        end
+        shelter3 = create(:shelter) do |shelter|
+          create_list(:pet, 5, shelter: shelter)
+        end
+
+        visit "/shelters"
+
+        click_button "Descending"
+
+        within ".shelters li:first-child" do
+          expect(page).to have_content(shelter3.name)
+          expect(page).to have_content(shelter3.pets.count)
+        end
+
+        within ".shelters li:nth-child(2)" do
+          expect(page).to have_content(shelter1.name)
+          expect(page).to have_content(shelter1.pets.count)
+        end
+
+        within ".shelters li:last-child" do
+          expect(page).to have_content(shelter2.name)
+          expect(page).to have_content(shelter2.pets.count)
         end
       end
     end
